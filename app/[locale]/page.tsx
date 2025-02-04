2fe4368584e9a17e0bc85736ae895beb17e09b3b
AdamskiJakub <jakub@mdataset.com>
2025-02-04 04:23:57 +0100

    fixing another issues

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,21 @@
 import { setRequestLocale } from "next-intl/server";
 import LocaleSwitcher from "@/components/switcher/LocaleSwitcher";
-import { Locale } from "@/i18n/routing";
+import { Locale, routing } from "@/i18n/routing";
 import prisma from "@/lib/prisma";
 import { Restaurant } from "@/types/restaurant";
 
-type Params = { locale: Locale };
+type Params = Promise<{ locale: Locale }>;
 
 interface Props {
   params: Params;
 }
 
-export const dynamic = "force-dynamic";
+export const generateStaticParams = (): { locale: Locale }[] => {
+  return routing.locales.map((locale) => ({ locale }));
+};
 
 export default async function HomePage({ params }: Props) {
-  const { locale } = params;
+  const { locale } = await params;
 
   setRequestLocale(locale);
 
