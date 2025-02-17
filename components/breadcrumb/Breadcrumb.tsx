22da0cf0afc87004a8dab554d80772d8d14b1e1f
AdamskiJakub <jakub@mdataset.com>
2025-02-17 18:47:50 +0100

    adding breadcrumb

---
+++ components/breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb";
+import { usePathname } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+
+const Breadcrumbs = () => {
+  const pathname = usePathname();
+  const t = useTranslations("Breadcrumb");
+
+  const pathSegments = pathname
+    .split("/")
+    .filter(
+      (segment) => segment !== "" && segment !== "pl" && segment !== "en"
+    );
+
+  if (pathSegments.length === 0) return null;
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/">{t("home")}</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+
+        {pathSegments.map((segment, index) => {
+          const href = `/${pathSegments.slice(0, index + 1).join("/")}`;
+          const isLast = index === pathSegments.length - 1;
+
+          return (
+            <span key={href} className="flex items-center">
+              <BreadcrumbSeparator>{`>`}</BreadcrumbSeparator>
+              <BreadcrumbItem>
+                {isLast ? (
+                  <BreadcrumbPage className="ml-3 text-gray">
+                    {t(segment)}
+                  </BreadcrumbPage>
+                ) : (
+                  <BreadcrumbLink asChild>
+                    <Link href={href}>{t(segment)}</Link>
+                  </BreadcrumbLink>
+                )}
+              </BreadcrumbItem>
+            </span>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default Breadcrumbs;
