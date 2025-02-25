e478aac762b22b2e1e9e21bb31eb8a408bdd664a
AdamskiJakub <jakub@mdataset.com>
2025-02-25 15:40:08 +0100

    fixing build

--- app/[locale]/restaurant/[slug]/page.tsx
+++ app/[locale]/restaurant/[slug]/page.tsx
@@ -2,12 +2,17 @@ import RestaurantDetails from "@/components/restaurant-page/RestaurantDetails";
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import { normalizeSlug } from "@/lib/normalizeSlug";
+import { Locale, routing } from "@/i18n/routing";
 
-type Params = Promise<{ slug: string }>;
+type Params = Promise<{ slug: string; locale: Locale }>;
 
 interface Props {
   params: Params;
 }
+
+export const generateStaticParams = (): { locale: Locale }[] => {
+  return routing.locales.map((locale) => ({ locale }));
+};
 
 export default async function RestaurantPage({ params }: Props) {
   const { slug } = await params;
