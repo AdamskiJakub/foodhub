0cdcdb9967f91084b606d08310d131624aa38585
AdamskiJakub <jakub@mdataset.com>
2025-02-25 13:54:41 +0100

    fixing comments

--- prisma/migrations/20250225021320_add_slug_to_restaurant/migration.sql
+++ prisma/migrations/20250225021320_add_slug_to_restaurant/migration.sql
@@ -11,15 +11,10 @@ ALTER TABLE "Restaurant" ADD COLUMN     "slug" TEXT NOT NULL;
 -- CreateIndex
 CREATE UNIQUE INDEX "Restaurant_slug_key" ON "Restaurant"("slug");
 
-
--- Krok 1: Dodaj kolumnę slug jako opcjonalną
 ALTER TABLE "Restaurant" ADD COLUMN "slug" TEXT;
 
--- Krok 2: Uzupełnij kolumnę slug na podstawie name (możesz użyć np. funkcji w SQL lub manualnie)
 UPDATE "Restaurant" SET "slug" = LOWER(REPLACE("name", ' ', '-')) WHERE "slug" IS NULL;
 
--- Krok 3: Zaktualizuj kolumnę slug, aby była NOT NULL
 ALTER TABLE "Restaurant" ALTER COLUMN "slug" SET NOT NULL;
 
--- Krok 4: Utwórz unikalny indeks na kolumnie slug
 CREATE UNIQUE INDEX "Restaurant_slug_key" ON "Restaurant"("slug");
