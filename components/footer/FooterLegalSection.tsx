5329d52d1700dc132d0c46c5f81b375c962f02dc
AdamskiJakub <105269736+AdamskiJakub@users.noreply.github.com>
2025-12-30 17:43:42 +0100

    feat: add contact page with project info and tech stack

--- components/footer/FooterLegalSection.tsx
+++ components/footer/FooterLegalSection.tsx
@@ -10,7 +10,7 @@ const FooterLegal = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between pb-8 pt-8 gap-3 px-4 md:px-10 lg:px-20 -mx-4 md:-mx-10 lg:-mx-20 border-t border-purple-200/50 mt-8">
+    <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between pb-8 pt-8 gap-3 border-t border-purple-200/50 mt-8">
       <p className="text-secondaryText text-xs">© {currentYear} FoodHub</p>
 
       <div className="flex items-center text-xs">
